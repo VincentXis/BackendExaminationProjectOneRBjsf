334fc361040e65bf4b5c6fe07388e2641325b829
VincentXis <rickard.bernal@gmail.com>
2017-05-22 15:41:08 +0200

    search page added, now working on editable tables

--- src/main/java/se/nackademin/addressbook/controller/ListContactsController.java
+++ src/main/java/se/nackademin/addressbook/controller/ListContactsController.java
@@ -1,5 +1,6 @@
 package se.nackademin.addressbook.controller;
 
+import org.primefaces.push.annotation.PathParam;
 import se.nackademin.addressbook.model.Contact;
 import se.nackademin.addressbook.model.ContactDetails;
 import se.nackademin.addressbook.service.AddressBookService;
@@ -7,25 +8,20 @@ import se.nackademin.addressbook.service.AddressBookService;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.Map;
 
 @Named
 @RequestScoped
 public class ListContactsController implements Serializable {
     @Inject
     private AddressBookService service;
-//    @PostConstruct
-//    private void load(){
-//        service.add(new ContactDetails("wawa","gaga","mail","123"));
-//        service.add(new ContactDetails("lala","sasa","mail","123"));
-//        service.add(new ContactDetails("gogo","nono","mail","123"));
-//    }
 
     public Collection<Contact> getContacts() {
         return service.getAllContacts();
     }
-
 }
--- src/main/java/se/nackademin/addressbook/controller/SearchController.java
+++ src/main/java/se/nackademin/addressbook/controller/SearchController.java
@@ -1,4 +1,42 @@
 package se.nackademin.addressbook.controller;
 
+import com.sun.deploy.net.HttpRequest;
+import se.nackademin.addressbook.model.Contact;
+import se.nackademin.addressbook.service.AddressBookService;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.Map;
+
+@ManagedBean(name = "searchController")
+@RequestScoped
 public class SearchController {
+
+    @Inject
+    private AddressBookService service;
+    private String searchQuery;
+    private Collection<Contact> contacts;
+
+
+    public void search() {
+        contacts = service.getContactsMatchingSearchQuery(searchQuery);
+    }
+
+
+    public Collection<Contact> getContacts() {
+        return contacts;
+    }
+
+    public String getSearchQuery() {
+        return searchQuery;
+    }
+
+    public void setSearchQuery(String searchQuery) {
+        this.searchQuery = searchQuery;
+    }
 }
